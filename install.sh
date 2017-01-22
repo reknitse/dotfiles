b776e6721b8e8c27ab6383cebac352a80643a466
Gregor Hoffmann <gregor.hoffmann@gmx.de>
2017-01-22 01:15:21 +0100

    bug fix for no brunch in submodules

--- install.sh
+++ install.sh
@@ -3,9 +3,12 @@
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # Update all submodules
-git submodule update --init --recursive
+git submodule update --remote vim/vim/bundle/Vundle.vim
+git submodule foreach -q --recursive 'branch="$(git config -f $toplevel/.gitmodules submodule.$name.branch)"; git checkout $branch'
 vim +PluginInstall +qall
 vim +PluginUpdate +qall
+git submodule update --init --remote --recursive
+git submodule foreach -q --recursive 'branch="$(git config -f $toplevel/.gitmodules submodule.$name.branch)"; git checkout $branch'
 
 "$BASEDIR"/link_dotfiles.sh
 
