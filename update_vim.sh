896a9920b80f264e7424e1c9186b82113cc5f996
Gregor Hoffmann <gregor.hoffmann@gmx.de>
2017-09-12 10:17:55 +0200

    added new submudule jedi-vim for python completition

--- update_vim.sh
+++ update_vim.sh
@@ -2,31 +2,6 @@
 
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-plugin_dir="$BASEDIR/vim/vim/bundle"
-if [[ ! -d "$plugin_dir" ]]; then
-    mkdir "$plugin_dir"
-fi
+# Update all submodules
+git submodule update --init --recursive
 
-function update_dir {
-    DIR="$plugin_dir/$(cut -d '/' -f 2 <<< $1)"
-    if [[ -d "$DIR" ]];then
-        rm "$DIR" -rf
-    fi
-    git clone "https://github.com/$1" "$DIR"
-    rm "$DIR"/.git -rf
-}
-
-update_dir tpope/vim-pathogen
-rm -rf "$plugin_dir/../autoload"
-mv "$plugin_dir/vim-pathogen/autoload" "$plugin_dir/../"
-rm -rf "$plugin_dir/vim-pathogen"
-update_dir easymotion/vim-easymotion
-update_dir vim-scripts/restore_view.vim
-update_dir Yggdroot/indentLine
-update_dir scrooloose/syntastic
-update_dir tomtom/tcomment_vim
-update_dir tpope/vim-surround
-update_dir vim-airline/vim-airline
-update_dir altercation/vim-colors-solarized
-update_dir sjl/badwolf
-update_dir tpope/vim-fugitive
